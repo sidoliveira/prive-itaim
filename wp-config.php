b99bce1ceb8b80df5e8f4dec7fa1d8dd4bc78e71
Sidney Oliveira <sidney-santana@live.com>
2021-07-26 14:15:03 -0300

    insere o tema prive

---
+++ wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * As configurações básicas do WordPress
+ *
+ * O script de criação wp-config.php usa esse arquivo durante a instalação.
+ * Você não precisa user o site, você pode copiar este arquivo
+ * para "wp-config.php" e preencher os valores.
+ *
+ * Este arquivo contém as seguintes configurações:
+ *
+ * * Configurações do MySQL
+ * * Chaves secretas
+ * * Prefixo do banco de dados
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/pt-br:Editando_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** Configurações do MySQL - Você pode pegar estas informações
+// com o serviço de hospedagem ** //
+/** O nome do banco de dados do WordPress */
+define('DB_NAME', 'prive');
+
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'prive250');
+
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', 'prive250321');
+
+/** Nome do host do MySQL */
+define('DB_HOST', 'mysql796.umbler.com');
+
+/** Charset do banco de dados a ser usado na criação das tabelas. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Chaves únicas de autenticação e salts.
+ *
+ * Altere cada chave para um frase única!
+ * Você pode gerá-las
+ * usando o {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org
+ * secret-key service}
+ * Você pode alterá-las a qualquer momento para desvalidar quaisquer
+ * cookies existentes. Isto irá forçar todos os
+ * usuários a fazerem login novamente.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'GGZDW?{{,-0XY<*HB1#<)n~*&?zSy]JNsc|O}3|_HQ-e7=6|$y8J.M8<9w*-mDEc');
+define('SECURE_AUTH_KEY',  'SWtF:v@]32)gA0(R]*D#bIX[QOA#BCo~@k,K^MJ0%_m]?&6HykgZ_8cx3epkM]?J');
+define('LOGGED_IN_KEY',    'yqi<ERx W.w$E,T:hW?+6yaVM0Z1k3t&74Gv2D_^`sJVW91w0%}@#t!w1QB%}Xjp');
+define('NONCE_KEY',        'xp+!SRgCm;T;+=P7e~K,T7=Lir?kzhP~0CjqVB;gaEaT1=AaliJc[N84_z*-udPn');
+define('AUTH_SALT',        'r@Lcc5J>SVI3;M/w$0-I!v#6(;%C8vJr<iQmnkZ3>J)r(=tCluF><,9jKtorbz:0');
+define('SECURE_AUTH_SALT', 'C$O(r(8(@XGX4s=(Fg9k1`jk8jrr.le=U`Y%xcLGDvffWB0Tg{oOK4)ZWw4uz&[ ');
+define('LOGGED_IN_SALT',   '!.>x[Vm7gvU3Tr{RE[~is-YG(xWNfGq@p47w$gCQRXHtBYQL0G=8mtL(+L(?nPcE');
+define('NONCE_SALT',       '8]`sg&R* OnnTeI?QZpr(eQQ;quXf=8Nwy5$khxPX)ne V)Kc;A;=|9uXnIdwxn}');
+
+/**#@-*/
+
+/**
+ * Prefixo da tabela do banco de dados do WordPress.
+ *
+ * Você pode ter várias instalações em um único banco de dados se você der
+ * para cada um um único prefixo. Somente números, letras e sublinhados!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Para desenvolvedores: Modo debugging WordPress.
+ *
+ * Altere isto para true para ativar a exibição de avisos
+ * durante o desenvolvimento. É altamente recomendável que os
+ * desenvolvedores de plugins e temas usem o WP_DEBUG
+ * em seus ambientes de desenvolvimento.
+ *
+ * Para informações sobre outras constantes que podem ser utilizadas
+ * para depuração, visite o Codex.
+ *
+ * @link https://codex.wordpress.org/pt-br:Depura%C3%A7%C3%A3o_no_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Isto é tudo, pode parar de editar! :) */
+
+/** Caminho absoluto para o diretório WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Configura as variáveis e arquivos do WordPress. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/prive/template-parts/content-contato.php
@@ -0,0 +1,30 @@
+<div class="contato-infos">
+    <h1 class="title-page"><?php echo rwmb_meta('rw_title'); ?></h1>
+    <div class="container">
+        <div class="row contato-desc">
+            <p class="contato-titulo">ENDEREÇO</p>
+            <p><?php echo rwmb_meta('rw_endereco'); ?></p>
+        </div>
+        <div class="row contato-desc">
+            <p class="contato-titulo">TELEFONES</p>
+            <p>
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span> <?php echo rwmb_meta('rw_telefone'); ?></span>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                <span> <?php echo rwmb_meta('rw_whatsapp'); ?></span>
+            </p>
+        </div>
+        <div class="row contato-desc">
+            <p class="contato-titulo">HORÁRIO DE FUNCIONAMENTO</p>
+            <p>
+                <?php echo rwmb_meta('rw_horario-func'); ?>
+                &nbsp;-&nbsp;
+                <?php echo rwmb_meta('rw_horario-func-dom'); ?>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="google-maps">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.0399614315197!2d-46.40175128555973!3d-23.495070084715827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce63778ae2398d%3A0x55e3831ea5c415c1!2sR.+Itapirema%2C+250+-+Jardim+Aimore%2C+S%C3%A3o+Paulo+-+SP%2C+08110-470!5e0!3m2!1spt-BR!2sbr!4v1499039022396" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
